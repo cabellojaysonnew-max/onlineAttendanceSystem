d718c8e29e641f1e58314c93caa906d615c1de8a
cabellojaysonnew-max <cabello.jayson.new@gmail.com>
2026-03-01 01:10:25 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
 import { supabase } from "./supabase.js";
 
-const API = "http://YOUR_PC_IP:8000/login";
-
+/* LOGIN USING SUPABASE AUTH */
 window.login = async function () {
 
-  const emp_id = document.getElementById("emp").value;
+  const emp = document.getElementById("emp").value;
   const password = document.getElementById("pass").value;
 
-  const res = await fetch(API,{
-    method:"POST",
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({emp_id,password})
+  const email = emp + "@lgu.local";
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password
   });
 
-  const data = await res.json();
-
-  if(data.success){
-    localStorage.setItem("employee",
-      JSON.stringify(data.employee));
-    location="dashboard.html";
-  }else{
+  if (error) {
     alert("Invalid login");
+    return;
   }
+
+  localStorage.setItem("user", JSON.stringify(data.user));
+  window.location = "dashboard.html";
 };
 
+
+/* CLOCK IN WITH GPS */
 window.clockIn = async function(){
 
   navigator.geolocation.getCurrentPosition(async (pos)=>{
 
-    const employee =
-      JSON.parse(localStorage.getItem("employee"));
+    const user = JSON.parse(localStorage.getItem("user"));
 
     const record = {
-      emp_id: employee.emp_id,
+      emp_id: user.email.split("@")[0],
       latitude: pos.coords.latitude,
       longitude: pos.coords.longitude,
       log_time: new Date(),
@@ -50,6 +49,7 @@ window.clockIn = async function(){
   });
 };
 
+
 async function upload(data){
   const { error } = await supabase
     .from("attendance_logs")
@@ -60,13 +60,17 @@ async function upload(data){
   }
 }
 
+
 function saveOffline(log){
   let logs =
     JSON.parse(localStorage.getItem("offlineLogs")) || [];
+
   logs.push(log);
+
   localStorage.setItem("offlineLogs",
     JSON.stringify(logs));
 }
+
 
 window.addEventListener("online", syncOffline);
 
